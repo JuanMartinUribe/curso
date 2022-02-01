7d4e2514257cd146e8068a577336362e7d1bf630
jmuribe10 <juanmartin104@gmail.com>
2022-02-01 09:53:46 -0500

    first commit

---
+++ program.php
@@ -0,0 +1,34 @@
+<?php 
+
+$first_name = "juan";
+
+
+$conn = mysqli_connect("localhost","root","","practica");
+
+$nameTable = "CREATE TABLE IF NOT EXISTS person (
+        name VARCHAR(30) NOT NULL)
+    "
+;
+    
+if ($conn->query($nameTable)===TRUE){
+    echo "connection successful";
+}   
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    $first_name = mysqli_real_escape_string($conn,$_REQUEST['name']);
+
+}
+
+$insert = "INSERT INTO PERSON(name)
+    VALUES ('$first_name')
+"
+;
+
+if($conn->query($insert)===true){
+    echo "data saved";
+}
+else{
+    echo "ERROR";
+}
+
+?>
+
